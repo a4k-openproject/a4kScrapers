62bbe46c114d9b798381ae477b4b9cff239e08e3
Newton Scamander <47229722+newt-sc@users.noreply.github.com>
2019-03-20 19:42:56 +0000

    fix: bitlord size

--- providers/a4kScrapers/en/torrent/bitlord.py
+++ providers/a4kScrapers/en/torrent/bitlord.py
@@ -5,3 +5,19 @@ from providerModules.a4kScrapers import core
 class sources(core.DefaultSources):
     def __init__(self):
         super(sources, self).__init__(__name__)
+
+    def info(self, el, url, torrent):
+        try:
+            size = int(el.size.replace('MB', ''))
+            if size == 0:
+                return
+            else:
+                if size < 120:
+                    size = size * 1024
+                elif size > 122880:
+                    size = int(size / 1024)
+        except: pass
+
+        el.size = '%s MB' % size
+
+        return self.genericScraper.info(el, url, torrent)
