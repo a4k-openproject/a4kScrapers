1a53940c7f9c9d973a1175c31a3dc90cdc12534a
Newton Scamander <47229722+newt-sc@users.noreply.github.com>
2019-07-09 21:02:41 +0000

    fix: cached provider does not support episodes

--- providers/a4kScrapers/en/torrent/cached.py
+++ providers/a4kScrapers/en/torrent/cached.py
@@ -5,3 +5,6 @@ from providerModules.a4kScrapers import core
 class sources(core.DefaultSources):
     def __init__(self):
         super(sources, self).__init__(__name__)
+
+    def episode(self, simple_info, all_info):
+        return []
