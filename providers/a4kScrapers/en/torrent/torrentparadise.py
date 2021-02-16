c250737f7d355e1392b9f76e778037e0751d7a8a
newt-sc <47229722+newt-sc@users.noreply.github.com>
2021-02-17 01:22:17 +0200

    release: v2.55.0

--- providers/a4kScrapers/en/torrent/torrentparadise.py
+++ providers/a4kScrapers/en/torrent/torrentparadise.py
@@ -4,7 +4,7 @@ from providerModules.a4kScrapers import core
 
 class sources(core.DefaultSources):
     def __init__(self, *args, **kwargs):
-        super(sources, self).__init__(__name__, *args, single_query=True, **kwargs)
+        super(sources, self).__init__(__name__, *args, **kwargs)
 
     def _search_request(self, url, query):
         response = super(sources, self)._search_request(url, query)
